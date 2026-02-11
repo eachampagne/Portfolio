bba4f44d844ec56b759ec0714e85470d147a9f78
Elizabeth Champagne <64558721+eachampagne@users.noreply.github.com>
2026-02-11 17:04:00 -0600

    preliminary hologram style for UI eleements

--- client/App.tsx
+++ client/App.tsx
@@ -3,8 +3,10 @@ import { useState } from 'react';
 function App() {
 
   return (
-    <div>
-      <h1 className="text-white">Rendering!</h1>
+    <div className='flex'>
+      <div className='hologram w-64 h-96 flex-1 m-8'>
+        <h1 className='text-white'>Rendering!</h1>
+      </div>
     </div>
   )
 }
